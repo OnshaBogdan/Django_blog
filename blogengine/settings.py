e6f350d0af6e3ae4fc04118b02190b16ea25c2cb
onsha.bogdan <onsha.bogdan2000@gmail.com>
2018-09-21 01:27:38 +0300

    Small security changes

--- blogengine/settings.py
+++ blogengine/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-lmk(%myr*2wowez#2)j$7$o)unzie3ywdkjc#iljde=u=+p8k'
+with open('blog/etc/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
